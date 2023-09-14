529d37307b0b56f6a4ea9c539ea36e11a19e5ad5
Tirth Patel <Tirthp0112@gmail.com>
2023-09-14 11:06:11 -0500

    Initial Commit

---
+++ UTOGETHER/UTOGETHERApp.swift
@@ -0,0 +1,17 @@
+//
+//  UTOGETHERApp.swift
+//  UTOGETHER
+//
+//  Created by Tirth D. Patel on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct UTOGETHERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
