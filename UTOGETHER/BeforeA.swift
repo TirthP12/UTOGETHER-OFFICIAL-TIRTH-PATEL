26f1816d8868605c935650f25222791892c53b87
Tirth Patel <67210830+TirthP12@users.noreply.github.com>
2023-10-24 11:14:32 -0500

    Update BeforeA.swift

--- UTOGETHER/BeforeA.swift
+++ UTOGETHER/BeforeA.swift
@@ -191,7 +191,7 @@ struct BeforeA: View {
                 Spacer()
                 
                 RoundedRectangle(cornerRadius: 20)
-                    .fill(Color.lightBlue)
+                    .fill(Color.gray)
                     .frame(width: 370, height: 200)
                     .overlay(
                         VStack(alignment: .center) {
@@ -241,64 +241,64 @@ struct BeforeA: View {
             
             Spacer()
             
-        }
-        HStack {
-            Spacer()
             
-            RoundedRectangle(cornerRadius: 20)
-                .fill(Color.lightBlue)
-                .frame(width: 370, height: 200)
-                .overlay(
-                    VStack(alignment: .center) {
-                        Text("Maeve O'Grady")
-                            .foregroundColor(.white)
-                            .font(.system(size: 20))
-                            .multilineTextAlignment(.center)
-                        
-                        Text("English, Psychology 1 & 2")
-                            .foregroundColor(.white)
-                            .font(.system(size: 15))
-                            .multilineTextAlignment(.center)
-                        
-                        VStack(alignment: .leading) {
-                            DatePicker("Select Date", selection: $selectedDate, displayedComponents: .date)
-                                .padding(.top, 10)
+            HStack {
+                Spacer()
+                
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.navy)
+                    .frame(width: 370, height: 200)
+                    .overlay(
+                        VStack(alignment: .center) {
+                            Text("Maeve O'Grady")
                                 .foregroundColor(.white)
+                                .font(.system(size: 20))
+                                .multilineTextAlignment(.center)
                             
-                            HStack {
-                                Button(action: {
-                                    let email = "mogrady4337@stu.d214.org"
-                                    let subject = "Tutoring Request - Before School on A day \(formattedDate(from: selectedDate))?"
-                                    let body = "Hello, can you help me on \(formattedDate(from: selectedDate))? Thank you!"
-                                    
-                                    let encodedSubject = subject.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
-                                    let encodedBody = body.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
-                                    let urlString = "mailto:\(email)?subject=\(encodedSubject)&body=\(encodedBody)"
-                                    if let url = URL(string: urlString) {
-                                        UIApplication.shared.open(url)
+                            Text("English, Psychology 1 & 2")
+                                .foregroundColor(.white)
+                                .font(.system(size: 15))
+                                .multilineTextAlignment(.center)
+                            
+                            VStack(alignment: .leading) {
+                                DatePicker("Select Date", selection: $selectedDate, displayedComponents: .date)
+                                    .padding(.top, 10)
+                                    .foregroundColor(.white)
+                                
+                                HStack {
+                                    Button(action: {
+                                        let email = "mogrady4337@stu.d214.org"
+                                        let subject = "Tutoring Request - Before School on A day \(formattedDate(from: selectedDate))?"
+                                        let body = "Hello, can you help me on \(formattedDate(from: selectedDate))? Thank you!"
+                                        
+                                        let encodedSubject = subject.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+                                        let encodedBody = body.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+                                        let urlString = "mailto:\(email)?subject=\(encodedSubject)&body=\(encodedBody)"
+                                        if let url = URL(string: urlString) {
+                                            UIApplication.shared.open(url)
+                                        }
+                                    }) {
+                                        Text("Request")
+                                            .foregroundColor(.black)
+                                            .padding(.vertical, 5)
+                                            .padding(.horizontal, 10)
+                                            .background(Color.white)
+                                            .cornerRadius(5)
                                     }
-                                }) {
-                                    Text("Request")
-                                        .foregroundColor(.black)
-                                        .padding(.vertical, 5)
-                                        .padding(.horizontal, 10)
-                                        .background(Color.white)
-                                        .cornerRadius(5)
                                 }
                             }
+                            .padding()
                         }
-                        .padding()
-                    }
-                )
+                    )
+                
+                Spacer()
+            }
             
             Spacer()
+            
         }
-        
-        Spacer()
-        
     }
 }
-
         
     
     private func formattedDate(from date: Date) -> String {
