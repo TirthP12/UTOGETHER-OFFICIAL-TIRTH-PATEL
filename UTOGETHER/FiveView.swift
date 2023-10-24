8d4dd17b4d4a3a4a0599537277f4df90f912d58d
Tirth Patel <Tirthp0112@gmail.com>
2023-10-24 11:15:06 -0500

    Update FiveView.swift

--- UTOGETHER/FiveView.swift
+++ UTOGETHER/FiveView.swift
@@ -186,6 +186,60 @@ struct FiveView: View {
                 }
                 
                 Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color.gray)
+                        .frame(width: 370, height: 200)
+                        .overlay(
+                            VStack(alignment: .center) {
+                                Text("Carola Adriante")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 20))
+                                    .multilineTextAlignment(.center)
+                                
+                                Text("AP Italian (Italian Exchange Student - 12/22/2023)")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 15))
+                                    .multilineTextAlignment(.center)
+                                
+                                VStack(alignment: .leading) {
+                                    DatePicker("Select Date", selection: $selectedDate, displayedComponents: .date)
+                                        .padding(.top, 10)
+                                        .foregroundColor(.white)
+                                    
+                                    HStack {
+                                        Button(action: {
+                                            let email = "cadriante4629@stu.d214.org"
+                                            let subject = "Tutoring Request - Block 5 \(formattedDate(from: selectedDate))?"
+                                            let body = "Hello, can you help me on \(formattedDate(from: selectedDate))? Thank you!"
+                                            
+                                            let encodedSubject = subject.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+                                            let encodedBody = body.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+                                            let urlString = "mailto:\(email)?subject=\(encodedSubject)&body=\(encodedBody)"
+                                            if let url = URL(string: urlString) {
+                                                UIApplication.shared.open(url)
+                                            }
+                                        }) {
+                                            Text("Request")
+                                                .foregroundColor(.black)
+                                                .padding(.vertical, 5)
+                                                .padding(.horizontal, 10)
+                                                .background(Color.white)
+                                                .cornerRadius(5)
+                                        }
+                                    }
+                                }
+                                .padding()
+                            }
+                        )
+                    
+                    Spacer()
+                }
+                
+                Spacer()
             }
             HStack {
                 Spacer()
@@ -243,10 +297,11 @@ struct FiveView: View {
         }
         
     }
+}
     private func formattedDate(from date: Date) -> String {
         let formatter = DateFormatter()
         formatter.dateStyle = .medium
         return formatter.string(from: date)
     }
-}
+
 
