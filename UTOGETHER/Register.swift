6283fdfccf0929bdbd1fce666a8fcac3f9f6e809
Tirth Patel <Tirthp0112@gmail.com>
2024-05-02 11:21:12 -0500

    dsda

---
+++ UTOGETHER/Register.swift
@@ -0,0 +1,18 @@
+//
+//  Register.swift
+//  UTOGETHER
+//
+//  Created by Tirth D. Patel on 5/2/24.
+//
+
+import SwiftUI
+
+struct Register: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Register()
+}
