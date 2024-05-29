0f714a6a4f15ee113591c99c87f0dff9b1059256
Tirth Patel <Tirthp0112@gmail.com>
2024-05-29 10:00:51 -0500

    dssd

---
+++ UTOGETHER/SearchBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBar.swift
+//  UTOGETHER
+//
+//  Created by Tirth D. Patel on 5/20/24.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBar()
+}
